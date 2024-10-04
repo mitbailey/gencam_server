9c78051926e0dfdf9aad0012b207d20048fc76ea
Mit Bailey <mitbailey@outlook.com>
2024-10-04 10:17:29 -0400

    Added documentation, cleaned up prints.

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,62 @@
 #![allow(clippy::unwrap_used, clippy::disallowed_methods)] // We are just testing here.
+/*!
+ * # GenCam Server
+ * This crate is an example of the client-facing portion of a Generic Camera-compatible server. This example loads an image from the file system, serializes it, and sends it using a GenCamPacket. The server listens for incoming connections and responds to requests for images.
+ */
 
 use std::{net::TcpListener, thread::spawn};
 use refimage::DynamicImageOwned;
 use image::open;
-use serde::{Serialize, Deserialize};
 use gencam_packet::{GenCamPacket, PacketType};
 
-fn load_and_transmit_image(path: &str, websocket: &mut tungstenite::WebSocket<std::net::TcpStream>, i: i32) {
+/// Loads a static image from path and sends it over websocket.
+/// Packet is serialized using serde_json.
+fn load_and_transmit_debug_image(path: &str, websocket: &mut tungstenite::WebSocket<std::net::TcpStream>, i: i32) {
+    // Load the image as a DynamicImage.
     let img = open(path).expect("Could not load image");
 
+    // This allows different image data to be sent using the same image. The hue is adjusted as per the passed i argument.
     let img = img.huerotate(90 * i);
 
-    let img = DynamicImageOwned::try_from(img).expect("Could not convert image");
+    // Converts the DynamicImage to DynamicImageOwned.
+    let img = DynamicImageOwned::try_from(img).expect("Could not convert image.");
 
+    // Create a new GenCamPacket with the image data.
     let pkt = GenCamPacket::new(PacketType::Image, 0, 256, 256, Some(img.as_raw_u8().to_vec()));
-    // Set msg to serialized pkt.
+    // Set msg to the serialized pkt.
     let msg = serde_json::to_vec(&pkt).unwrap();
-    // Send
+    // Send the message.
     websocket.send(msg.into()).unwrap(); 
-    // websocket.send(img.as_raw_u8().into()).expect("Could not send image");
 }
 
 fn main() {
+    // Counter to track how many images have been sent.
     let mut i = 0;
+
+    // Set up websocket listening.
     let bind_addr = "127.0.0.1:9001";
     let server = TcpListener::bind(bind_addr).unwrap();
     eprintln!("Listening on: ws://{bind_addr}");
+
+    // When we get an incoming client connection...
     for stream in server.incoming() {
         spawn(move || {
             let mut websocket = tungstenite::accept(stream.unwrap()).unwrap();
-            eprintln!("New client connected");
+            eprintln!("New client connected!");
+
+            // When we receive a message...
             while let Ok(msg) = websocket.read() {
+                // Only do something if its a binary message that is a deserializable GenCamPacket.
                 if let tungstenite::Message::Binary(data) = msg {
                     let pkt: GenCamPacket = serde_json::from_slice(&data).unwrap();
                     match pkt.packet_type {
+                        // If we received an image request, load and transmit the debug image.
                         PacketType::ImgReq => {
                             i += 1;
-                            load_and_transmit_image("res/test_image_1.png", &mut websocket, i);
+                            load_and_transmit_debug_image("res/test_image_1.png", &mut websocket, i);
                             eprintln!("Responded (ImgReq #{i})");
                         },
+                        // Right now, we do not handle any other packet types.
                         _ => {
                             let pkt = GenCamPacket::new(PacketType::Ack, 0, 0, 0, None);
                             // Set msg to serialized pkt.
@@ -48,23 +66,9 @@ fn main() {
                         },
                     }
                 }
-
-                // // We do not want to send back ping/pong messages.
-                // if msg.is_binary() || msg.is_text() {
-                //     if let Err(err) = websocket.send(msg.clone()) {
-                //         eprintln!("Error sending message: {err}");
-                //         break;
-                //     } else if msg.is_text() && msg.to_text().unwrap() == "send test image" {
-                //         load_and_transmit_image("res/test_image_1.png", &mut websocket);   
-                //         eprintln!("Responded (send test image): {msg}");
-                //     } else {
-                //         eprintln!("Responded: {msg}");
-                //     }
-                // } else {
-                //     eprintln!("Message received not text or binary.");
-                // }
             }
-            eprintln!("Client left.");
+
+            eprintln!("Client disconnected.");
         });
     }
 }
